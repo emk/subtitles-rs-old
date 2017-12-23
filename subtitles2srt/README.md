1386f3c4be1afcb41b9aa8d9c9b840753e41aa8d
Eric Kidd <git@randomhacks.net>
2017-12-23 11:53:05 -0500

    Replace source code with stub pointing to merged project

--- subtitles2srt/README.md
+++
@@ -1,3 +0,0 @@
-# `subtitles2srt`: Convert Vobsub (sub/idx) files to SRT text subtitles
-
-**This is not yet in a usable state.**
--- vobsub/src/mpeg2/mod.rs
+++
@@ -1,8 +0,0 @@
-//! The `*.sub` portion of VobSub subtitles is packaged in MPEG-2 Program
-//! Stream packets, which we have some limited support for parsing.
-
-mod clock;
-pub mod pes;
-pub mod ps;
-
-pub use self::clock::*;
